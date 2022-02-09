8e0ef8d1f94b42ca4a0446e166ff526cce6fe4cb
Ferdzen <fernandaivp63@gmail.com>
2022-02-09 15:56:51 -0300

    Grafo e pesquisa em largura - função complementada

--- bookTests/t3.py
+++ bookTests/t3.py
@@ -17,17 +17,26 @@ grafo["julien"] = []
 grafo["yeri"] = []
 grafo["seulgi"] = []
 
-fila_de_pesquisa = deque()
-fila_de_pesquisa += grafo["voce"]
 
-#função para determinar qual é o vendedor de manga. Determinou que seria a pessoa cujo nome terminasse com M.
+
+#função para determinar qual é o vendedor de manga.
 def pessoa_e_vendedor(nome):
-    return nome[-1] == 'm'
+    return nome[-1] == 'm' #nome que termine com a letra setada
 
+def pesquisa(nome):
+    fila_de_pesquisa = deque()
+    fila_de_pesquisa += grafo["voce"]
+    verificadas = []
 
-while fila_de_pesquisa:
-    pessoa = fila_de_pesquisa.popleft() #pega a primeira pessoa da fila
-    if pessoa_e_vendedor(pessoa): #verifica se essa pessoa vende manga
-        print(pessoa +' é vendedor de manga!') #sim, ela vende manga
-    else:
-        fila_de_pesquisa += grafo[pessoa] #não, ela nao vende manga. adiciona todos os amigos dessa pessoa à lista
+    while fila_de_pesquisa:
+        pessoa = fila_de_pesquisa.popleft() #pega a primeira pessoa da fila
+        if not pessoa in verificadas: #verifica se o nome já não foi checado
+            if pessoa_e_vendedor(pessoa): #verifica se essa pessoa vende manga
+                print(pessoa +' é vendedor de manga!') #sim, ela vende manga
+                return True #finaliza programa
+            else:
+                fila_de_pesquisa += grafo[pessoa] #não, ela nao vende manga. adiciona todos os amigos dessa pessoa à lista
+                verificadas.append(pessoa) #adiciona ao vetor o nome verificado para que não seja feita checagem 2 vezes
+    return False
+
+pesquisa("voce")
